c05bf26b3ef54dc8d529415f4d15614c4c9be4d0
Suman-MLops <1DS23CD404@dsce.edu.in>
2025-07-02 08:39:27 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,5 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
-x,y= load_iris(return_x_y=True)
+iris= load_iris()
+x=iris.data
+y=iris.target
 model=LogisticRegression(max_iter=200).fit(x,y)
 print("model_trained")
